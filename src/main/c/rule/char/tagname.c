61a9b71ea5c510ad6906376597410f8f874680ca
Wilson Lin <contact@wilsonl.in>
2018-07-04 00:17:41 +1200

    Tag name rules

---
+++ src/main/c/rule/char/tagname.c
@@ -0,0 +1,22 @@
+#ifndef _HDR_HYPERBUILD_RULE_TAGNAME
+#define _HDR_HYPERBUILD_RULE_TAGNAME
+
+#include "../../ext/nicehash/src/main/c/set/int32.h"
+#include "./lcalpha.c"
+#include "./ucalpha.c"
+#include "./digit.c"
+
+static nh_set_int32_t hb_r_tagname_set;
+
+void hb_r_tagname_init() {
+  hb_r_tagname_set = nh_set_int32_create();
+  hb_r_lcalpha_add_elems(hb_r_tagname_set);
+  hb_r_ucalpha_add_elems(hb_r_tagname_set);
+  hb_r_digit_add_elems(hb_r_tagname_set);
+}
+
+int hb_r_tagname_check(char c) {
+  return nh_set_int32_has(hb_r_tagname_set, c);
+}
+
+#endif // _HDR_HYPERBUILD_RULE_TAGNAME
