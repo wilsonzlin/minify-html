0626dd92ba91699a6e6c1e13b2fe0f34a93650f8
Wilson Lin <contact@wilsonl.in>
2018-07-04 00:17:52 +1200

    Unquoted attribute value rules

---
+++ src/main/c/rule/char/unquotedattrval.c
@@ -0,0 +1,24 @@
+#ifndef _HDR_HYPERBUILD_RULE_UNQUOTEDATTRVAL
+#define _HDR_HYPERBUILD_RULE_UNQUOTEDATTRVAL
+
+#include "../../ext/nicehash/src/main/c/set/int32.h"
+#include "./whitespace.c"
+
+static nh_set_int32_t hb_r_unquotedattrval_set;
+
+void hb_r_unquotedattrval_init() {
+  hb_r_unquotedattrval_set = nh_set_int32_create();
+  hb_r_whitespace_add_elems(hb_r_unquotedattrval_set);
+  nh_set_int32_add(hb_r_unquotedattrval_set, '"');
+  nh_set_int32_add(hb_r_unquotedattrval_set, '\'');
+  nh_set_int32_add(hb_r_unquotedattrval_set, '`');
+  nh_set_int32_add(hb_r_unquotedattrval_set, '=');
+  nh_set_int32_add(hb_r_unquotedattrval_set, '<');
+  nh_set_int32_add(hb_r_unquotedattrval_set, '>');
+}
+
+int hb_r_unquotedattrval_check(char c) {
+  return nh_set_int32_has(hb_r_unquotedattrval_set, c);
+}
+
+#endif // _HDR_HYPERBUILD_RULE_UNQUOTEDATTRVAL
