0df89c76139cad65f3ad87e6dfdacff669a74bee
Wilson Lin <contact@wilsonl.in>
2018-07-04 23:26:07 +1200

    Compile script

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd "$(dirname "$0")" > /dev/null
+
+mkdir -p out
+
+gcc --std=c99 -Wall -Werror -O2 -o out/hyperbuild src/main/c/main.c
+
+popd > /dev/null
+
+exit 0
