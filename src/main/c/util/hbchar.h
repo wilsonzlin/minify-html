a818e4f95ab7aa94e21078c267b1a44f2074a445
Wilson Lin <contact@wilsonl.in>
2018-07-04 23:24:12 +1200

    Set up common C char types and defs

---
+++ src/main/c/util/hbchar.h
@@ -0,0 +1,10 @@
+#ifndef _HDR_HYPERBUILD_UTIL_HBCHAR
+#define _HDR_HYPERBUILD_UTIL_HBCHAR
+
+typedef int hb_eod_char_t;
+typedef unsigned char hb_char_t;
+
+#define HB_EOD -1 // End Of Data
+#define SIZEOF_CHAR sizeof(hb_char_t)
+
+#endif // _HDR_HYPERBUILD_UTIL_HBCHAR
