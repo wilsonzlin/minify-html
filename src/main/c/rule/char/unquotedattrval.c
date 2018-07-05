26ddcc2cc6bc67095765d571ddad8db5cbdcba61
Wilson Lin <contact@wilsonl.in>
2018-07-05 19:51:25 +1200

    Fix unquoted attribute rules (blacklist instead of whitelist)

--- src/main/c/rule/char/unquotedattrval.c
+++ src/main/c/rule/char/unquotedattrval.c
@@ -5,20 +5,21 @@
 #include "../../util/hbchar.h"
 #include "./whitespace.c"
 
-static nh_set_int32_t hb_r_unquotedattrval_set;
+static nh_set_int32_t hb_r_unquotedattrval_blacklist;
 
 void hb_r_unquotedattrval_init(void) {
-  hb_r_unquotedattrval_set = nh_set_int32_create();
-  hb_r_whitespace_add_elems(hb_r_unquotedattrval_set);
-  nh_set_int32_add(hb_r_unquotedattrval_set, '"');
-  nh_set_int32_add(hb_r_unquotedattrval_set, '\'');
-  nh_set_int32_add(hb_r_unquotedattrval_set, '`');
-  nh_set_int32_add(hb_r_unquotedattrval_set, '=');
-  nh_set_int32_add(hb_r_unquotedattrval_set, '<');
-  nh_set_int32_add(hb_r_unquotedattrval_set, '>');
+  hb_r_unquotedattrval_blacklist = nh_set_int32_create();
+  hb_r_whitespace_add_elems(hb_r_unquotedattrval_blacklist);
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '"');
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '\'');
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '`');
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '=');
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '<');
+  nh_set_int32_add(hb_r_unquotedattrval_blacklist, '>');
 }
 
 int hb_r_unquotedattrval_check(hb_char_t c) {
+  return !nh_set_int32_has(hb_r_unquotedattrval_blacklist, c);
 }
 
 #endif // _HDR_HYPERBUILD_RULE_UNQUOTEDATTRVAL
