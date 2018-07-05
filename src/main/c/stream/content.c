e2aa3fa6f54c4df8afdd049948f56765672fe997
Wilson Lin <contact@wilsonl.in>
2018-07-05 17:00:39 +1200

    Start content stream

---
+++ src/main/c/stream/content.c
@@ -0,0 +1,34 @@
+#ifndef _HDR_HYPERBUILD_STREAM_CONTENT
+#define _HDR_HYPERBUILD_STREAM_CONTENT
+
+#include "../util/hbchar.h"
+#include "../util/pipe.c"
+
+#include "./tag.c"
+
+void hbs_content(hbu_pipe_t pipe) {
+  hb_eod_char_t c = hbu_pipe_peek_eoi(pipe);
+
+  switch (c) {
+  case HB_EOD:
+    return;
+
+  case '<':
+    if (hbu_pipe_peek_offset(pipe, 2) == '/') {
+      // Callee is responsible for requiring close tag (or not, if root)
+      return;
+    }
+
+    hbs_tag(pipe);
+    break;
+
+  case '&':
+    // TODO
+    break;
+
+  default:
+    hbu_pipe_accept(pipe);
+  }
+}
+
+#endif // _HDR_HYPERBUILD_STREAM_CONTENT
