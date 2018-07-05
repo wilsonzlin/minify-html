c73de842724a3c022c74356f5439590433cb2e4f
Wilson Lin <contact@wilsonl.in>
2018-07-05 19:40:27 +1200

    Compile using C11 standard

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ pushd "$(dirname "$0")" > /dev/null
 
 mkdir -p out
 
-gcc --std=c99 -Wall -Wextra -Werror -O2 -o out/hyperbuild src/main/c/main.c
+gcc --std=c11 -Wall -Wextra -Werror -O2 -o out/hyperbuild src/main/c/main.c
 
 popd > /dev/null
 
