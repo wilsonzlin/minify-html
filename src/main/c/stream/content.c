1c637e6a9479b4e35b0343438cbfdbe34d7bb643
Wilson Lin <contact@wilsonl.in>
2018-07-05 20:56:49 +1200

    Fix missing loop in content stream

--- src/main/c/stream/content.c
+++ src/main/c/stream/content.c
@@ -10,27 +10,29 @@ void hbs_content(hbu_pipe_t pipe);
 #include "./tag.c"
 
 void hbs_content(hbu_pipe_t pipe) {
-  hb_eod_char_t c = hbu_pipe_peek_eoi(pipe);
+  while (1) {
+    hb_eod_char_t c = hbu_pipe_peek_eoi(pipe);
 
-  switch (c) {
-  case HB_EOD:
-    return;
-
-  case '<':
-    if (hbu_pipe_peek_offset(pipe, 2) == '/') {
-      // Callee is responsible for requiring close tag (or not, if root)
+    switch (c) {
+    case HB_EOD:
       return;
+
+    case '<':
+      if (hbu_pipe_peek_offset(pipe, 2) == '/') {
+        // Callee is responsible for requiring close tag (or not, if root)
+        return;
+      }
+
+      hbs_tag(pipe);
+      break;
+
+    case '&':
+      // TODO
+      break;
+
+    default:
+      hbu_pipe_accept(pipe);
     }
-
-    hbs_tag(pipe);
-    break;
-
-  case '&':
-    // TODO
-    break;
-
-  default:
-    hbu_pipe_accept(pipe);
   }
 }
 
