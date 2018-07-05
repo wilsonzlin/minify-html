cc3b047e36b144d9ab47b6c4c890977c6d3460cf
Wilson Lin <contact@wilsonl.in>
2018-07-05 23:57:07 +1200

    Content tags

---
+++ src/main/c/rule/tag/contenttags.c
@@ -0,0 +1,35 @@
+#ifndef _HDR_HYPERBUILD_RULE_CONTENTTAGS
+#define _HDR_HYPERBUILD_RULE_CONTENTTAGS
+
+#include "../../ext/nicehash/set/str.h"
+#include "../../util/hbchar.h"
+
+static nh_set_str_t hb_r_contenttags_set;
+
+void hb_r_contenttags_add_elems(nh_set_str_t set) {
+  nh_set_str_add(set, "address");
+  nh_set_str_add(set, "audio");
+  nh_set_str_add(set, "button");
+  nh_set_str_add(set, "canvas");
+  nh_set_str_add(set, "caption");
+  nh_set_str_add(set, "figcaption");
+  nh_set_str_add(set, "legend");
+  nh_set_str_add(set, "meter");
+  nh_set_str_add(set, "object");
+  nh_set_str_add(set, "option");
+  nh_set_str_add(set, "p");
+  nh_set_str_add(set, "summary"); // or a heading
+  nh_set_str_add(set, "textarea");
+  nh_set_str_add(set, "video");
+}
+
+void hb_r_contenttags_init(void) {
+  hb_r_contenttags_set = nh_set_str_create();
+  hb_r_contenttags_add_elems(hb_r_contenttags_set);
+}
+
+int hb_r_contenttags_check(hb_char_t *tag) {
+  return nh_set_str_has(hb_r_contenttags_set, (char *) tag);
+}
+
+#endif // _HDR_HYPERBUILD_RULE_CONTENTTAGS
