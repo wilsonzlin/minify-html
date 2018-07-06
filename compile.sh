d11e8d21b14c1edf83a4cc541ba15a0e9d05b98a
Wilson Lin <contact@wilsonl.in>
2018-07-06 22:59:14 +1200

    Add debug mode to compile script

--- compile.sh
+++ compile.sh
@@ -6,7 +6,11 @@ pushd "$(dirname "$0")" > /dev/null
 
 mkdir -p out
 
-gcc --std=c11 -Wall -Wextra -Werror -O2 -o out/hyperbuild src/main/c/main.c
+if [ "$1" = "--debug" ]; then
+  gcc --std=c11 -Wall -Wextra -Werror -O0 -g -o out/hyperbuild src/main/c/main.c
+else
+  gcc --std=c11 -Wall -Wextra -Werror -O2 -o out/hyperbuild src/main/c/main.c
+fi
 
 popd > /dev/null
 
