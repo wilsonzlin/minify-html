9321e2769b04ca1bda4ed90debac094006d0fc19
Wilson Lin <code@wilsonl.in>
2019-12-30 19:35:56 +1100

    Clean up Node.js module code

--- nodejs/native/src/lib.rs
+++ nodejs/native/src/lib.rs
@@ -1,12 +1,9 @@
-#[macro_use]
-extern crate neon;
-
 use neon::prelude::*;
 use hyperbuild::hyperbuild;
 
 fn minify(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let mut buffer = cx.argument::<JsBuffer>(0)?;
-    match cx.borrow_mut(&mut buffer, |mut code| hyperbuild(code.as_mut_slice::<u8>())) {
+    match cx.borrow_mut(&mut buffer, |code| hyperbuild(code.as_mut_slice::<u8>())) {
         Ok(out_len) => Ok(cx.number(out_len as f64)),
         Err((err, pos)) => cx.throw_error(format!("{} [Character {}]", err.message(), pos)),
     }
