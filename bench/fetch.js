82a2e6e9833efd3c56cd14c4edc9864c24faef68
Wilson Lin <code@wilsonl.in>
2020-01-09 01:00:05 +1100

    Update bench tests and results

--- bench/fetch.js
+++ bench/fetch.js
@@ -4,26 +4,26 @@ const path = require('path');
 const prettier = require('prettier');
 
 const tests = {
-  "Amazon": "https://amazon.com/",
-  "BBC": "https://bbc.co.uk/",
+  "Amazon": "https://www.amazon.com/",
+  "BBC": "https://www.bbc.co.uk/",
   "Bootstrap": "https://getbootstrap.com/docs/3.4/css/",
-  "Bing": "https://bing.com/",
+  "Bing": "https://www.bing.com/",
   "Coding Horror": "https://blog.codinghorror.com/",
-  "ECMA-262": "https://tc39.es/ecma262/",
-  "Google": "https://google.com/",
+  "Google": "https://www.google.com/",
   "Hacker News": "https://news.ycombinator.com/",
-  "NY Times": "https://nytimes.com/",
-  "Reddit": "https://reddit.com/",
-  "Stack Overflow": "https://stackoverflow.com/",
+  "NY Times": "https://www.nytimes.com/",
+  "Reddit": "https://www.reddit.com/",
+  "Stack Overflow": "https://www.stackoverflow.com/",
   "Twitter": "https://twitter.com/",
-  "Wikipedia": "https://en.wikipedia.org/wiki/Soil/",
-  "Yahoo": "https://yahoo.com/",
+  "Wikipedia": "https://en.wikipedia.org/wiki/Soil",
 };
 
 const fetchTest = async (name, url) => {
   const html = await request({
     url,
+    gzip: true,
     headers: {
+      'Accept': '*/*',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; rv:71.0) Gecko/20100101 Firefox/71.0',
     },
   });
@@ -37,7 +37,29 @@ const fetchTest = async (name, url) => {
 
   // Format after fetching as formatting is synchronous and can take so long that connections get dropped by server due to inactivity.
   for (const [name, html] of await Promise.all(Object.entries(tests).map(([name, url]) => fetchTest(name, url)))) {
-    const formatted = prettier.format(html, {parser: 'html'});
+    // Apply some fixes to HTML to allow strict formatter to work.
+    const formatted = prettier.format(
+      html
+        // Fix missing semicolon after entity in Amazon.
+        .replace(/&#x200b/g, '&#x200b;')
+        // Fix consecutive malformed entities in Amazon.
+        .replace(/&& window.ue_sbl/g, '&amp&amp window.ue_sbl')
+        .replace(/&&pf_rd_p/g, '&amp&amppf_rd_p')
+        // Fix early termination of conditional comment in Amazon.
+        .replace('--></style>\n<![endif]-->', '</style>\n<![endif]-->')
+        // Fix closing of void tag in Amazon.
+        .replace(/><\/hr>/g, '/>')
+        // Fix extra '</div>' in BBC.
+        .replace('</a></span></small></div></div></div></footer>', '</a></span></small></div></div></footer>')
+        // Fix consecutive malformed entities in Google.
+        .replace(/&&google.aft/g, '&amp&ampgoogle.aft')
+        // Fix parser failing to parse unquoted attribute value starting with forward slash in Stack Overflow.
+        .replace('action=/search', 'action="/search"')
+        // Fix broken attribute value in Stack Overflow.
+        .replace('height=151"', 'height="151"')
+      ,
+      {parser: 'html'},
+    );
     console.log(`Formatted ${name}`);
     await fs.writeFile(path.join(__dirname, 'tests', `${name}.html`), formatted);
   }
