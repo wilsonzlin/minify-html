574fee36a7a38bc8a92a48caed1cfcbfcc9cb5df
Wilson Lin <code@wilsonl.in>
2020-01-18 16:54:52 +1100

    Update version script and set up build pipeline for Java

--- java/src/main/java/in/wilsonl/hyperbuild/Hyperbuild.java
+++ java/src/main/java/in/wilsonl/hyperbuild/Hyperbuild.java
@@ -2,12 +2,33 @@ package in.wilsonl.hyperbuild;
 
 import java.util.Arrays;
 
+import static in.wilsonl.hyperbuild.NativeLibraryLoader.loadLibraryFromJar;
+import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Hyperbuild {
   static {
-    String nativeLibPath = Hyperbuild.class.getResource("libhyperbuild_java.so").getPath();
-    System.load(nativeLibPath);
+    String osName = System.getProperty("os.name").toLowerCase();
+    String osArch = System.getProperty("os.arch").toLowerCase();
+
+    String nativeLibNameOs = osName.startsWith("windows")
+        ? "windows"
+        : osName.startsWith("linux")
+            ? "linux"
+            : osName.startsWith("mac")
+                ? "macos"
+                : null;
+    String nativeLibNameArch = osArch.equals("amd64") ? "x86_64" : null;
+
+    if (nativeLibNameOs == null || nativeLibNameArch == null) {
+      throw new RuntimeException(format("Platform not supported (%s, %s)", osName, osArch));
+    }
+
+    try {
+      loadLibraryFromJar(format("%s-%s.nativelib", nativeLibNameOs, nativeLibNameArch));
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to load native library", e);
+    }
   }
 
   public static native int minifyInPlace(byte[] code) throws HyperbuildException;
