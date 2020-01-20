d520897cb2b2a04e26375772453870cebc7fd12a
Wilson Lin <code@wilsonl.in>
2020-01-21 00:50:20 +1100

    0.0.35

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hyperbuild",
-    version="0.0.34",
+    version="0.0.35",
     author="Wilson Lin",
     author_email="code@wilsonl.in",
     description="Fast one-pass in-place HTML minifier written in Rust with context-aware whitespace handling",
