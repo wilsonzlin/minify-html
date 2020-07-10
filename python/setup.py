e21889dd498366981398ce77dfe9dd517a55772d
Wilson Lin <code@wilsonl.in>
2020-07-11 01:51:00 +1000

    0.2.1

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hyperbuild",
-    version="0.2.0",
+    version="0.2.1",
     author="Wilson Lin",
     author_email="code@wilsonl.in",
     description="Fast allocation-less HTML minifier with smart whitespace handling",
