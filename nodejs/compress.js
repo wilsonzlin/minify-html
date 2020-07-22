697823ab17b92f94b875e48f34f2066f21c67188
Wilson Lin <code@wilsonl.in>
2020-07-22 21:38:54 +1000

    Use Node.js native module directly without JS overhead

--- nodejs/compress.js
+++ nodejs/compress.js
@@ -2,6 +2,6 @@ const {readFileSync, writeFileSync} = require('fs');
 const {join} = require('path');
 const {gzipSync} = require('zlib');
 
-const src = readFileSync(join(__dirname, 'dist', 'native.node'));
+const src = readFileSync(join(__dirname, 'dist', 'index.node'));
 const out = gzipSync(src);
-writeFileSync(join(__dirname, 'dist', 'native.node.gz'), out);
+writeFileSync(join(__dirname, 'dist', 'index.node.gz'), out);
