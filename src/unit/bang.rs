2eb1bea07738d3b43d8a8826b24e6d870c9d13d3
Wilson Lin <code@wilsonl.in>
2020-07-27 18:09:49 +1000

    Bang -> bang

--- src/unit/bang.rs
+++ src/unit/bang.rs
@@ -6,6 +6,6 @@ use crate::proc::Processor;
 #[inline(always)]
 pub fn process_bang(proc: &mut Processor) -> ProcessingResult<()> {
     proc.m(IsSeq(b"<!"), Keep).expect();
-    proc.m(ThroughChar(b'>'), Keep).require("Bang close")?;
+    proc.m(ThroughChar(b'>'), Keep).require("bang close")?;
     Ok(())
 }
