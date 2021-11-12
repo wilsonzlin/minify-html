266e876dacadf5f0fd718f853e45bc068d957e29
Wilson Lin <code@wilsonl.in>
2021-11-12 17:24:06 +1100

    0.7.0

--- nodejs/package.json.gen.js
+++ nodejs/package.json.gen.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const common = {
-  version: "0.6.10",
+  version: "0.7.0",
   main: "index.node",
   types: "index.d.ts",
   files: ["cli.js", "postinstall.js", "index.d.ts"],
