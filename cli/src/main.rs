7c5aa713708b5620b55eaed0db1c9577f446a658
Jason Yundt <swagfortress@gmail.com>
2022-01-21 16:49:24 -0500

    Allow CLI to minify CSS and JS

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,9 +1,51 @@
+use std::fmt;
 use std::fs::File;
 use std::io::{stdin, stdout, Read, Write};
+use std::str::FromStr;
 
 use structopt::StructOpt;
 
-use minify_html::{minify, Cfg};
+use minify_html::{minify, minify_css, minify_js, Cfg};
+
+#[derive(PartialEq)]
+enum SourceType {
+    Html,
+    Css,
+    Js,
+}
+
+impl Default for SourceType {
+    fn default() -> Self {
+        SourceType::Html
+    }
+}
+
+impl fmt::Display for SourceType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SourceType::Html => "html",
+                SourceType::Css => "css",
+                SourceType::Js => "js",
+            }
+        )
+    }
+}
+
+impl FromStr for SourceType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "html" => Ok(SourceType::Html),
+            "css" => Ok(SourceType::Css),
+            "js" => Ok(SourceType::Js),
+            _ => Err(format!("\"{}\" is not \"html\", \"css\" or \"js\".", s)),
+        }
+    }
+}
 
 #[derive(StructOpt)]
 #[structopt(
@@ -20,11 +62,11 @@ struct Cli {
     #[structopt(short, long, parse(from_os_str))]
     output: Option<std::path::PathBuf>,
 
-    /// Minify JS in `<script>` tags that have a valid or no `type` attribute value.
+    /// Minify Js in `<script>` tags that have a valid or no `type` attribute value.
     #[structopt(long)]
     minify_js: bool,
 
-    /// Minify CSS in `<style>` tags and `style` attributes.
+    /// Minify Css in `<style>` tags and `style` attributes.
     #[structopt(long)]
     minify_css: bool,
 
@@ -44,7 +86,7 @@ struct Cli {
     #[structopt(long)]
     keep_html_and_head_opening_tags: bool,
 
-    /// Keep spaces between attributes when possible to conform to HTML standards.
+    /// Keep spaces between attributes when possible to conform to Html standards.
     #[structopt(long)]
     keep_spaces_between_attributes: bool,
 
@@ -59,6 +101,10 @@ struct Cli {
     /// Remove all processing_instructions.
     #[structopt(long)]
     remove_processing_instructions: bool,
+
+    /// Allowed values: "html", "css" or "js" (not case-sensitive).
+    #[structopt(default_value, long)]
+    source_type: SourceType,
 }
 
 macro_rules! io_expect {
@@ -85,22 +131,24 @@ fn main() {
         src_file.read_to_end(&mut src_code),
         "could not load source code"
     );
-    let out_code = minify(
-        &src_code,
-        &Cfg {
-            do_not_minify_doctype: args.do_not_minify_doctype,
-            ensure_spec_compliant_unquoted_attribute_values: args
-                .ensure_spec_compliant_unquoted_attribute_values,
-            keep_closing_tags: args.keep_closing_tags,
-            keep_comments: args.keep_comments,
-            keep_html_and_head_opening_tags: args.keep_html_and_head_opening_tags,
-            keep_spaces_between_attributes: args.keep_spaces_between_attributes,
-            minify_css: args.minify_css,
-            minify_js: args.minify_js,
-            remove_bangs: args.remove_bangs,
-            remove_processing_instructions: args.remove_processing_instructions,
-        },
-    );
+    let cfg = &Cfg {
+        do_not_minify_doctype: args.do_not_minify_doctype,
+        ensure_spec_compliant_unquoted_attribute_values: args
+            .ensure_spec_compliant_unquoted_attribute_values,
+        keep_closing_tags: args.keep_closing_tags,
+        keep_comments: args.keep_comments,
+        keep_html_and_head_opening_tags: args.keep_html_and_head_opening_tags,
+        keep_spaces_between_attributes: args.keep_spaces_between_attributes,
+        minify_css: args.minify_css | (args.source_type == SourceType::Css),
+        minify_js: args.minify_js | (args.source_type == SourceType::Js),
+        remove_bangs: args.remove_bangs,
+        remove_processing_instructions: args.remove_processing_instructions,
+    };
+    let out_code = match args.source_type {
+        SourceType::Html => minify(&src_code, cfg),
+        SourceType::Css => minify_css(&src_code, cfg),
+        SourceType::Js => minify_js(&src_code, cfg),
+    };
     let mut out_file: Box<dyn Write> = match args.output {
         Some(p) => Box::new(io_expect!(File::create(p), "could not open output file")),
         None => Box::new(stdout()),
