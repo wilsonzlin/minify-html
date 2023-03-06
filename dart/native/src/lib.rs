6af6286f0a31968d5426128cfb9fec231f974d21
Mensch <mensch272@users.noreply.github.com>
2023-03-06 19:35:54 +0500

    Add rust extern minify function

--- dart/native/src/lib.rs
+++ dart/native/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::slice;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use minify_html::Cfg;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[no_mangle]
+pub extern "C" fn minify_html(
+    input: *const u8,
+    length: usize,
+    do_not_minify_doctype: bool,
+    ensure_spec_compliant_unquoted_attribute_values: bool,
+    keep_closing_tags: bool,
+    keep_html_and_head_opening_tags: bool,
+    keep_spaces_between_attributes: bool,
+    keep_comments: bool,
+    minify_css: bool,
+    minify_css_level_1: bool,
+    minify_css_level_2: bool,
+    minify_css_level_3: bool,
+    minify_js: bool,
+    remove_bangs: bool,
+    remove_processing_instructions: bool,
+) {
+    let src = unsafe { slice::from_raw_parts(input, length) };
+
+    let cfg = Cfg {
+        do_not_minify_doctype,
+        ensure_spec_compliant_unquoted_attribute_values,
+        keep_closing_tags,
+        keep_html_and_head_opening_tags,
+        keep_spaces_between_attributes,
+        keep_comments,
+        minify_css,
+        minify_css_level_1,
+        minify_css_level_2,
+        minify_css_level_3,
+        minify_js,
+        remove_bangs,
+        remove_processing_instructions,
+    };
+
+    minify_html::minify(src, &cfg);
 }
