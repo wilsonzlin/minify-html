bd2dabfa2e14d5031a51c55e28411c44191ed0d5
Wilson Lin <code@wilsonl.in>
2023-12-24 10:19:38 +1100

    [Ruby] Use hash for Cfg param

--- minify-html-ruby/ext/minify_html/src/lib.rs
+++ minify-html-ruby/ext/minify_html/src/lib.rs
@@ -1,41 +1,26 @@
 use magnus::define_global_function;
 use magnus::function;
+use magnus::RHash;
+use magnus::StaticSymbol;
 use minify_html::minify as minify_html_native;
 use minify_html::Cfg as CfgNative;
 
-#[magnus::wrap(class = "Cfg")]
-struct Cfg {
-  do_not_minify_doctype: bool,
-  ensure_spec_compliant_unquoted_attribute_values: bool,
-  keep_closing_tags: bool,
-  keep_html_and_head_opening_tags: bool,
-  keep_spaces_between_attributes: bool,
-  keep_comments: bool,
-  minify_css: bool,
-  minify_css_level_1: bool,
-  minify_css_level_2: bool,
-  minify_css_level_3: bool,
-  minify_js: bool,
-  remove_bangs: bool,
-  remove_processing_instructions: bool,
-}
-
-fn minify_html(source: String, cfg: &Cfg) -> String {
+fn minify_html(source: String, cfg: RHash) -> String {
+  #[rustfmt::skip]
   let out_code = minify_html_native(source.as_bytes(), &CfgNative {
-    do_not_minify_doctype: cfg.do_not_minify_doctype,
-    ensure_spec_compliant_unquoted_attribute_values: cfg
-      .ensure_spec_compliant_unquoted_attribute_values,
-    keep_closing_tags: cfg.keep_closing_tags,
-    keep_html_and_head_opening_tags: cfg.keep_html_and_head_opening_tags,
-    keep_spaces_between_attributes: cfg.keep_spaces_between_attributes,
-    keep_comments: cfg.keep_comments,
-    minify_css: cfg.minify_css,
-    minify_css_level_1: cfg.minify_css_level_1,
-    minify_css_level_2: cfg.minify_css_level_2,
-    minify_css_level_3: cfg.minify_css_level_3,
-    minify_js: cfg.minify_js,
-    remove_bangs: cfg.remove_bangs,
-    remove_processing_instructions: cfg.remove_processing_instructions,
+    do_not_minify_doctype: cfg.aref(StaticSymbol::new("do_not_minify_doctype")).unwrap_or_default(),
+    ensure_spec_compliant_unquoted_attribute_values: cfg.aref(StaticSymbol::new("ensure_spec_compliant_unquoted_attribute_values")).unwrap_or_default(),
+    keep_closing_tags: cfg.aref(StaticSymbol::new("keep_closing_tags")).unwrap_or_default(),
+    keep_html_and_head_opening_tags: cfg.aref(StaticSymbol::new("keep_html_and_head_opening_tags")).unwrap_or_default(),
+    keep_spaces_between_attributes: cfg.aref(StaticSymbol::new("keep_spaces_between_attributes")).unwrap_or_default(),
+    keep_comments: cfg.aref(StaticSymbol::new("keep_comments")).unwrap_or_default(),
+    minify_css: cfg.aref(StaticSymbol::new("minify_css")).unwrap_or_default(),
+    minify_css_level_1: cfg.aref(StaticSymbol::new("minify_css_level_1")).unwrap_or_default(),
+    minify_css_level_2: cfg.aref(StaticSymbol::new("minify_css_level_2")).unwrap_or_default(),
+    minify_css_level_3: cfg.aref(StaticSymbol::new("minify_css_level_3")).unwrap_or_default(),
+    minify_js: cfg.aref(StaticSymbol::new("minify_js")).unwrap_or_default(),
+    remove_bangs: cfg.aref(StaticSymbol::new("remove_bangs")).unwrap_or_default(),
+    remove_processing_instructions: cfg.aref(StaticSymbol::new("remove_processing_instructions")).unwrap_or_default(),
   });
   String::from_utf8(out_code).unwrap()
 }
