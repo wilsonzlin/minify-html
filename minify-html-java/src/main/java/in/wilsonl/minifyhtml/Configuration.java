137ba670713ef457bbb2210b277977a0a7443842
jogerj <30559735+jogerj@users.noreply.github.com>
2025-06-28 02:07:27 +0200

    feat: use a value class for config
    Signed-off-by: jogerj <30559735+jogerj@users.noreply.github.com>

--- minify-html-java/src/main/java/in/wilsonl/minifyhtml/Configuration.java
+++ minify-html-java/src/main/java/in/wilsonl/minifyhtml/Configuration.java
@@ -3,30 +3,32 @@ package in.wilsonl.minifyhtml;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Value;
 
 /**
  * Class representing minification configuration.
  * Use the {@link Builder} to create an instance of this class.
  */
-@Builder(
-        setterPrefix = "set",
-        builderClassName = "Builder"
-)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(
+    setterPrefix = "set",
+    builderClassName = "Builder"
+)
+@Value
 public class Configuration {
-    public final boolean allowNoncompliantUnquotedAttributeValues;
-    public final boolean allowOptimalEntities;
-    public final boolean allowRemovingSpacesBetweenAttributes;
-    public final boolean keepClosingTags;
-    public final boolean keepComments;
-    public final boolean keepHtmlAndHeadOpeningTags;
-    public final boolean keepInputTypeTextAttr;
-    public final boolean keepSsiComments;
-    public final boolean minifyCss;
-    public final boolean minifyDoctype;
-    public final boolean minifyJs;
-    public final boolean preserveBraceTemplateSyntax;
-    public final boolean preserveChevronPercentTemplateSyntax;
-    public final boolean removeBangs;
-    public final boolean removeProcessingInstructions;
+    boolean allowNoncompliantUnquotedAttributeValues;
+    boolean allowOptimalEntities;
+    boolean allowRemovingSpacesBetweenAttributes;
+    boolean keepClosingTags;
+    boolean keepComments;
+    boolean keepHtmlAndHeadOpeningTags;
+    boolean keepInputTypeTextAttr;
+    boolean keepSsiComments;
+    boolean minifyCss;
+    boolean minifyDoctype;
+    boolean minifyJs;
+    boolean preserveBraceTemplateSyntax;
+    boolean preserveChevronPercentTemplateSyntax;
+    boolean removeBangs;
+    boolean removeProcessingInstructions;
 }
